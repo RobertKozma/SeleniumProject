49a0942a67364c31023dfe06a092a35308a82102
Robert Kozma <robert.kozma@endava.com>
2025-01-10 14:45:17 +0200

    added sleep times for the test steps

--- src/test/java/Tests/PracticeFormTest.java
+++ src/test/java/Tests/PracticeFormTest.java
@@ -20,10 +20,12 @@ public class PracticeFormTest {
         // accesam o pagina web
         driver.get("https://demoqa.com/");
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         // punem browser-ul in modul maximize
         driver.manage().window().maximize();
+
+        Thread.sleep(1000);
 
         //facem un scroll
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -31,27 +33,42 @@ public class PracticeFormTest {
 
         // declaram un element
         WebElement formField=driver.findElement(By.xpath("//h5[text()='Forms']"));
-
         formField.click();
+
+        Thread.sleep(1000);
 
         WebElement practiceFormField=driver.findElement(By.xpath("//span[text()='Practice Form']"));
         practiceFormField.click();
+
+        Thread.sleep(1000);
+
+        js.executeScript("window.scrollBy(0,400)");
+
+        Thread.sleep(1000);
 
         WebElement firstNameField=driver.findElement(By.id("firstName"));
         String firstNameValue="Jon";
         firstNameField.sendKeys(firstNameValue);
 
+        Thread.sleep(500);
+
         WebElement lastNameField=driver.findElement(By.id("lastName"));
         String lastNameValue="Doe";
         lastNameField.sendKeys(lastNameValue);
+
+        Thread.sleep(500);
 
         WebElement emailField=driver.findElement(By.id("userEmail"));
         String emailNameValue="mail@mail.com";
         emailField.sendKeys(emailNameValue);
 
+        Thread.sleep(500);
+
         WebElement mobileNumberField=driver.findElement(By.cssSelector("input[placeholder='Mobile Number']"));
         String mobileNumberValue="0741356169";
         mobileNumberField.sendKeys(mobileNumberValue);
+
+        Thread.sleep(500);
 
         WebElement pictureField=driver.findElement(By.id("uploadPicture"));
         File file = new File("src/test/resources/download.jpg");
